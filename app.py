da084f22d5d7ac18464cc3985d13179a8404174c
Felix Fennell <felnne@bas.ac.uk>
2019-07-19 09:41:52 +0100

    Re-implementing ISO 19115 stylesheets with Flask
    Removes the need for Gulp

--- app.py
+++ app.py
@@ -10,14 +10,27 @@ def create_app():
     app = Flask(__name__)
 
     @app.route('/standards/iso-19115/<configuration>')
-    def standard_iso_19115(configuration: str):
+    @app.route('/standards/iso-19115/<configuration>/<stylesheet>')
+    def standard_iso_19115(configuration: str, stylesheet: str = None):
         if configuration == 'uk-pdc-candidate':
             configuration_object = metadata_record_configs.iso19115_v1_gemini_v2_3_uk_pdc_candidate
         else:
             return KeyError('Invalid configuration, valid options: [uk-pdc-candidate]')
 
         configuration = ISO19115MetadataRecordConfig(**configuration_object)
+
         record = ISO19115MetadataRecord(configuration)
+        if stylesheet is not None:
+            if stylesheet == 'iso-html':
+                record.record.addprevious(PI(
+                    'xml-stylesheet',
+                    'type="text/xsl" href="/static/xml-stylesheets/iso-html/xml-to-html-ISO.xsl"'
+                ))
+            elif stylesheet == 'iso-rubric':
+                record.record.addprevious(PI(
+                    'xml-stylesheet',
+                    'type="text/xsl" href="/static/xml-stylesheets/iso-rubric/isoRubricHTML.xsl"'
+                ))
 
         return Response(record.generate_xml_document(), mimetype='text/xml')
 
